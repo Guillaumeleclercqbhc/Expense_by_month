9a701a1d3b7151267a1f6bb045f83155a9cb1b13
Guillaume Leclercq <guillaume.leclercq@bhc.be>
2015-09-21 14:40:42 +0200

    add yml

--- tests/test_expense_month.py
+++ tests/test_expense_month.py
@@ -8,7 +8,35 @@ class Test_expense_month(common.TransactionCase):
     def setUp(self):
         """*****setUp*****"""
         super(Test_expense_month, self).setUp()
-        print "Testing "
+        self.expense_obj = self.registry('hr.expense.expense')
+        self.exp_line_obj = self.registry('hr.expense.line')
+        self.product_obj = self.registry('product.product')
+        self.tax_obj = self.registry('account.tax')
+        self.code_obj = self.registry('account.tax.code')
+        _, self.product_id = self.registry("ir.model.data").get_object_reference(cr, uid, "hr_expense", "air_ticket")
+        _, self.employee_id = self.registry("ir.model.data").get_object_reference(cr, uid, "hr", "employee_mit")
+        self.base_code_id = self.code_obj.create(cr, uid, {'name': 'Expense Base Code'})
+        self.tax_id = self.tax_obj.create(cr, uid, {
+            'name': 'Expense 10%',
+            'amount': 0.10,
+            'type': 'percent',
+            'type_tax_use': 'purchase',
+            'price_include': True,
+            'base_code_id': self.base_code_id,
+            'base_sign': -1,
+        })
+        self.product_obj.write(cr, uid, self.product_id, {'supplier_taxes_id': [(6, 0, [self.tax_id])]})
+        self.expense_id = self.expense_obj.create(cr, uid, {
+            'name': 'Expense for Minh Tran',
+            'employee_id': self.employee_id,
+            'months': 'Fevrier'
+        })
+        self.exp_line_obj.create(cr, uid, {
+            'name': 'Car Travel Expenses',
+            'product_id': self.product_id,
+            'unit_amount': 700.00,
+            'expense_id': self.expense_id
+        })
         '''
         self.demo_user = self.env['res.users'].search([('name', '=', 'Demo User')])
         self.product_bolognese_ref = self.env['ir.model.data'].get_object_reference('lunch', 'product_Bolognese')
